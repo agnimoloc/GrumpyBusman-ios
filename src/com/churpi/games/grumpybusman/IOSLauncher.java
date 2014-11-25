3ecbb28fb26b8cf7cde5f626ec2f25101b4259b8
Igor <paco.aval@gmail.com>
2014-11-24 22:20:42 -0600

    first load

---
+++ src/com/churpi/games/grumpybusman/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.churpi.games.grumpybusman;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.churpi.games.grumpybusman.GrumpyBusmanGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new GrumpyBusmanGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
